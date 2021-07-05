a0553a1d937025fb0f6c1ccf7e7aefa8cdb7b057
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-05 15:35:57 +0530

    Add files via upload

---
+++ DynamicArrayProgram/DynamicNumberArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicArrayProgram
+{
+    public class DynamicNumberArray
+    {
+
+        int[] buffer;
+        public DynamicNumberArray()
+        {
+            buffer = new int[5];
+        }
+        public DynamicNumberArray(int initialSize)
+        { buffer = new int[initialSize]; }
+
+
+        public int this[int index]
+        {
+            set
+            {
+                if(index >= buffer.Length)
+                {
+                    Array.Resize(ref buffer, index + 5);
+                }
+                buffer[index] = value;
+            }
+
+            get
+            {
+                return buffer[index];
+            }
+
+
+
+        }
+
+
+       
+       
+
+       
+    }
+}
+
